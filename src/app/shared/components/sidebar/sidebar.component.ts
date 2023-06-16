128a740421cdc14ce98f7e2be5c1e52510851fb8
Carlos Diaz <carlosdiaz.dev@gmail.com>
2023-06-16 13:52:02 -0600

    [FEAT] Angular aplication logic added

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+
+  constructor(private gifsServices: GifsService ){
+
+  }
+
+  get tags(): string[]{
+    return this.gifsServices.tagsHistory;
+  }
+
+  searchTag(tag: string):void{
+    this.gifsServices.searchTag(tag)
+  }
+
+}
